f5483cfbb8317aa2ce21b3daf4fdbf41dd2fb7eb
Lepper-Jackson <21029@fraser.school.nz>
2024-04-08 14:52:18 +1200

    Commit session 8th April

--- 91896&91897/main.py
+++ 91896&91897/main.py
@@ -1,7 +1,7 @@
 """This file calculates the most cost effective ingredient for them."""
 # functions within the code
 def budget(question):
-    # getting users budget
+# getting users budget
 
     while True:
         response = input('What is your budget: ')
@@ -10,13 +10,35 @@ def budget(question):
         if response < 1.65:
             print("Your budget is too low, please enter a higher number.")
 
-        elif response > 5:
+        elif response > 20:
             print("Your budget is too high, please enter a lower number.")
 
-        elif response >= 1.65 or response <= 5:
+        elif response >= 1.65 or response <= 20:
             continue
 
         else:
-            print("Please enter a valid number 1.65 - 5.")
+            print("Please enter a valid number 1.65 - 20.")
+            
+def product_name(question):
+    # getting users product name
+
+    response_pn = input("List your product's name: ")
+
+    if response_pn == str:
+        print("Added, were there anymore? ")
+
+        if response_pn == "yes".lower or "y".lower:
+    elif response_pn == int:
+        print("Please only characters in the english alphabet.")
+
+def weight_g(question):
+    # gettins users weight in grams
+def weight_kg
+def cost
+def unit_price
+
+ 
 
 # main routine
+
+print(" ")
